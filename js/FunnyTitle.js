4b077a2e5a47f2af570cc7fde7dddf72924b4543
rainemotion <3520377907@qq.com>
2023-11-23 17:43:00 +0800

    Site updated: 2023-11-23 17:43:00

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,15 +1,15 @@
 
- var OriginTitle = document.title;
+var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/joke.ico");
          document.title = 'ヽ(●-`Д´-)ノ404！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ汗流浃背了吧牢底！' + OriginTitle;
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = 'ヾ(Ő∀Ő3)ノ汗流浃背了吧牢底！';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
